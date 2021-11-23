66a826da8c9084f9e1178641c87a8119d06eba24
paragraut <parag.raut@outlook.com>
2021-11-23 12:23:38 +0100

    Initial Commit

--- Processor.UnitTests/TestData.cs
+++ Processor.UnitTests/TestData.cs
@@ -1,19 +1,18 @@
-﻿namespace Processor.UnitTests
+﻿namespace Processor.UnitTests;
+
+public class TestData
 {
-    public class TestData
-    {
-        public int Accumulator { get; set; }
+    public int Accumulator { get; set; }
 
-        public int XRegister { get; set; }
+    public int XRegister { get; set; }
 
-        public int YRegister { get; set; }
+    public int YRegister { get; set; }
 
-        public int StackPointer { get; set; }
+    public int StackPointer { get; set; }
 
-        public int Flags { get; set; }
+    public int Flags { get; set; }
 
-        public int CycleCount { get; set; }
+    public int CycleCount { get; set; }
 
-        public int ProgramCounter { get; set; }
-    }
+    public int ProgramCounter { get; set; }
 }
